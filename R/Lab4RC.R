43c75a0553fef8e13e70b1847bfe870dd2ff430c
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-09-22 22:41:41 +0200

    Changed license in description

--- R/Lab4RC.R
+++ R/Lab4RC.R
@@ -143,8 +143,6 @@ linreg <- setRefClass("linreg",
                             }
                             cat("",sep="\n")
                             cat(paste("Residual standard error: ",round(sqrt(Var_residuals),5) ," on " ,df, " degrees of freedom",sep=""))
-                            
-
                           } 
                         ))
 
