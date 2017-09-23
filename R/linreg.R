acb46d499cc8136618c36f5b02b0fe6f9f7202f6
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-09-23 11:43:32 +0200

    skeleton

--- R/linreg.R
+++ R/linreg.R
@@ -1,10 +1,18 @@
 
 
-#' @title Euclidean Algorithm Implementation
-#' @name  my_num_vector
-#' @description Euclidean Algorithm Implementation to find the Greatest Common Divisor(GCD) of two numbers one and two param
-#' @export
-my_num_vector <- function (){
-  e <- exp(1)
-  return (c(log10(11),cos(pi/5),e^(pi/3),1173%%7 /19)) 
-}
+#' #' @title Euclidean Algorithm Implementation
+#' #' @name  my_num_vector
+#' #' @description Euclidean Algorithm Implementation to find the Greatest Common Divisor(GCD) of two numbers one and two param
+#' #' @export
+#' my_num_vector <- function (){
+#'   e <- exp(1)
+#'   return (c(log10(11),cos(pi/5),e^(pi/3),1173%%7 /19)) 
+#' }
+
+#' @title Linear Regression
+#' @description You can have Reference Class containing some calculations by giving formula and data.
+#' @export linreg
+#' @exportClass linreg
+linreg <- setRefClass("linreg",
+                      fields = list(),
+                      methods = list())
